72cfa4193bc834056fa24df6f2458aa943d518b6
yumin <chinawym@gmail.com>
2013-12-11 23:27:31 +0800

    first

---
+++ src/main/java/me/yumin/java/common/util/ObjectUtil.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package me.yumin.java.common.util;
+
+/**
+ * @author yumin
+ * 
+ */
+public class ObjectUtil {
+
+    /**
+     * 如果对象为<code>null</code>，则返回指定默认对象，否则返回对象本身。
+     * <pre>
+     * ObjectUtil.defaultIfNull(null, null)      = null
+     * ObjectUtil.defaultIfNull(null, "")        = ""
+     * ObjectUtil.defaultIfNull(null, "zz")      = "zz"
+     * ObjectUtil.defaultIfNull("abc", *)        = "abc"
+     * ObjectUtil.defaultIfNull(Boolean.TRUE, *) = Boolean.TRUE
+     * </pre>
+     *
+     * @param object 要测试的对象
+     * @param defaultValue 默认值
+     *
+     * @return 对象本身或默认对象
+     */
+    public static Object defaultIfNull(Object object, Object defaultValue) {
+        return (object != null) ? object
+                                : defaultValue;
+    }
+}
