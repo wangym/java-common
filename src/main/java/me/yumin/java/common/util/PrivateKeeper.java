be8010b85e39bc55654709ffdc1c8f5f2c7f6525
yumin <chinawym@gmail.com>
2015-03-02 14:55:58 +0800

    add PrivateKeeper class

---
+++ src/main/java/me/yumin/java/common/util/PrivateKeeper.java
@@ -0,0 +1,9 @@
+package me.yumin.java.common.util;
+
+/**
+ * @author xuanyin
+ * @since 2015-03-02 14:52
+ */
+public class PrivateKeeper {
+    
+}
---
+++ src/test/java/me/yumin/java/common/util/test/PrivateKeeperTest.java
@@ -0,0 +1,9 @@
+package me.yumin.java.common.util.test;
+
+/**
+ * @author xuanyin
+ * @since 2015-03-02 14:53
+ */
+public class PrivateKeeperTest {
+
+}
