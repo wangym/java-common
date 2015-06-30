240e04846c2fab5d4a13dd60cd4b03faf3498794
yumin <chinawym@gmail.com>
2015-06-30 23:01:59 +0800

    add new methods

--- src/main/java/me/yumin/java/common/util/LogUtil.java
+++ src/main/java/me/yumin/java/common/util/LogUtil.java
@@ -18,17 +18,90 @@ public class LogUtil {
     }
 
     /**
-     * @param t throwable
+     * @param message
      */
-    public static void error(Throwable t) {
-        LOG.error(t);
+    public static void error(String message) {
+        error(message, null);
     }
 
     /**
-     * @param message make any content
-     * @param t       throwable
+     * @param t
      */
-    public static void error(Object message, Throwable t) {
+    public static void error(Throwable t) {
+        error(null, t);
+    }
+
+    /**
+     * @param message
+     * @param t
+     */
+    public static void error(String message, Throwable t) {
         LOG.error(message, t);
+    }
+
+    /**
+     * @param message
+     */
+    public static void warn(String message) {
+        warn(message, null);
+    }
+
+    /**
+     * @param t
+     */
+    public static void warn(Throwable t) {
+        warn(null, t);
+    }
+
+    /**
+     * @param message
+     * @param t
+     */
+    public static void warn(String message, Throwable t) {
+        LOG.warn(message, t);
+    }
+
+    /**
+     * @param message
+     */
+    public static void info(String message) {
+        info(message, null);
+    }
+
+    /**
+     * @param t
+     */
+    public static void info(Throwable t) {
+        info(null, t);
+    }
+
+    /**
+     * @param message
+     * @param t
+     */
+    public static void info(String message, Throwable t) {
+        LOG.info(message, t);
+    }
+
+    /**
+     * @param message
+     */
+    public static void debug(String message) {
+        debug(message, null);
+    }
+
+    /**
+     * @param t
+     */
+    public static void debug(Throwable t) {
+        debug(null, t);
+    }
+
+    /**
+     * @param message
+     * @param t
+     */
+    public static void debug(String message, Throwable t) {
+        LOG.debug(message, t);
     }
 }
